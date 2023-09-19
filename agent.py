a8a326a4d750a05871c2b6998df1cc4288386e33
Kishore S <kishore.bm19@bitsathy.ac.in>
2023-09-19 22:11:52 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -1,32 +1,30 @@
 import os
+import tempfile
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
-
 from langchain.chains import ConversationalRetrievalChain
 from langchain.llms import OpenAI
 
-
 class Agent:
     def __init__(self, openai_api_key: str = None) -> None:
-        # if openai_api_key is None, then it will look the enviroment variable OPENAI_API_KEY
+        # if openai_api_key is None, then it will look the environment variable OPENAI_API_KEY
         self.embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-
         self.llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
+        self.chat_history = []  # Initialize chat history to an empty list
 
-        self.chat_history = None
         self.chain = None
         self.db = None
 
-    def ask(self, question: str) -> str:
+    def ask(self, question: str, document_source: str) -> str:
         if self.chain is None:
             response = "Please, add a document."
         else:
             response = self.chain({"question": question, "chat_history": self.chat_history})
             response = response["answer"].strip()
-            self.chat_history.append((question, response))
+            self.chat_history.append((question, response, document_source))  # Add document source
         return response
 
     def ingest(self, file_path: os.PathLike) -> None:
@@ -37,11 +35,11 @@ class Agent:
         if self.db is None:
             self.db = FAISS.from_documents(splitted_documents, self.embeddings)
             self.chain = ConversationalRetrievalChain.from_llm(self.llm, self.db.as_retriever())
-            self.chat_history = []
         else:
             self.db.add_documents(splitted_documents)
 
     def forget(self) -> None:
         self.db = None
         self.chain = None
-        self.chat_history = None
+        self.chat_history = []  # Clear chat history when forgetting
+
